cedf6c1f58611db0baac04e518f6a686a285341a
chenlu-chua <chenlu.ccl@gmail.com>
2019-11-02 14:50:08 +0800

    fixed type

--- src/main/java/com/penniless/springboot/PennyWiserApplication.java
+++ src/main/java/com/penniless/springboot/PennyWiserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PennyWiserApplication {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         SpringApplication.run(PennyWiserApplication.class, args);
     }
 }
